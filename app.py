123ed0f7f56f89b871091b07496d8c8453f3e125
Ashok_kumar <74713336+ashok2216-A@users.noreply.github.com>
2024-05-01 20:43:32 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from transformers import pipeline
+import seaborn as sns
+
+sentiment_model = pipeline(model="ashok2216/gpt2-amazon-sentiment-classifier")
+
+sentiments = []
+for text in df['clean_text']:
+    if list(sentiment_model(text)[0].values())[0] == 'LABEL_1':
+        output = 'Positive'
+    else:
+        output = 'Negative'
+    sentiments.append(output)
+
+df['sentiments'] = sentiments
+sns.countplot(df['sentiments'])
